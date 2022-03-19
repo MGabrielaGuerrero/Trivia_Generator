9f7fbb6b4b8b9080689241ce41190a401e05838d
maria.guerrero <maria.guerrero@itconsultores.com.co>
2022-03-19 12:14:45 -0500

    [FEATURE] add page principal

---
+++ WebMosquera/back/db.js
@@ -0,0 +1,17 @@
+const { Sequileze } = require("sequelize");
+
+const sequelize = new Sequileze('id18034453_juegotrivia', 'id18034453_mosquera', '>CCT4z|Co0w4}Y<', {
+    host: "localhost",
+    dialect: 'mysql2'
+})
+
+    (async () => {
+        try {
+            await sequelize.authenticate();
+            console.log("conexcion a la BD Existosa")
+        } catch (err) {
+            console.error("Conexion a la BD fallida", err)
+        }
+    })
+
+module.exports = sequelize
---
+++ WebMosquera/back/index.js
@@ -0,0 +1,4 @@
+console.log("hola mundo")
+const express = require("express")
+
+const BaseDatos = require("./db")
